49ac7f7f1607209dbbb107e5c61712fc6ed14d2b
Everlyn <gachuieve32@gmail.com>
2024-01-11 18:50:32 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -20,19 +20,79 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = {
+            "id": bakery.id,
+            "name": bakery.name,
+            "created_at": bakery.created_at,
+            "updated_at": bakery.updated_at,
+        }
+        bakeries.append(bakery_dict)
+
+    response = make_response(
+        jsonify(bakeries),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        jsonify(bakery_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    # Convert the result to JSON
+    baked_goods_dict = [{
+        "id": baked_good.id,
+        "name": baked_good.name,
+        "price": baked_good.price,
+        "created_at" : baked_good.created_at,
+        "updated_at" : baked_good.updated_at
+    } for baked_good in baked_goods]
+
+    response = make_response(
+        jsonify(baked_goods_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    # Convert the result to JSON
+    baked_good_dict = {
+        "id": baked_good.id,
+        "name": baked_good.name,
+        "price": baked_good.price,
+        "created_at" : baked_good.created_at,
+        "updated_at" : baked_good.updated_at
+    }
+
+    response = make_response(
+        jsonify(baked_good_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
